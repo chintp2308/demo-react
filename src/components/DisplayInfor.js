21f9628f4ea51dc0e2a5a001e7317fc0b850e1f2
chintp2308 <nphuongchi23@gmail.com>
2025-02-24 15:48:39 +0700

    hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,106 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    super(props);
-    //babel compiler
-    this.state = {
-      showHide: true,
-    };
-  }
 
-  handleShowHide = () => {
-    this.setState({
-      showHide: !this.state.showHide,
-    });
-  };
-
-  //Component = Template JSX (HTML) + Logic Javascript
-  render() {
-    const { listUser } = this.props;
-    return (
-      <div className="display-infor-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.showHide === true ? "Hide" : "Show"}
-          </span>
-        </div>
-        {this.state.showHide && (
-          <div>
-            {listUser.map((user, index) => {
-              return (
-                <div key="id" className={+user.age > 18 ? "green" : "red"}>
-                  <div>
-                    <div>My name's {user.name}</div>
-                    <div>My name's {user.age}</div>
-                  </div>
-
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-export default DisplayInfor;
-
-// import React from "react";
-
+//stateless(ko có state) vs stateful(có state)
 // class DisplayInfor extends React.Component {
-//   state = {
-//     isShowListUser: true,
-//   };
-
-//   handleShowHide = () => {
-//     this.setState({
-//       isShowListUser: !this.state.isShowListUser,
-//     });
-//   };
-
+//   //Component = Template JSX (HTML) + Logic Javascript
 //   render() {
-//     //destructuring array/object
 //     const { listUser } = this.props;
-//     //props => viết tắt properties
 //     return (
-//       <div>
-//         <div>
-//           <span
-//             onClick={() => {
-//               this.handleShowHide();
-//             }}
-//           >
-//             Hide list user
-//             {
-//               (this.state.isShowListUser = true
-//                 ? " Hide list user"
-//                 : "Show list user")
-//             }
-//           </span>
-//         </div>
-//         {this.state.isShowListUser && (
+//       <div className="display-infor-container">
+//         {true && (
 //           <div>
 //             {listUser.map((user, index) => {
-//               console.log("check user", user);
 //               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name's {user.name}</div>
-//                   <div>My age's {user.age}</div>
-//                   <hr />
+//                 <div key="id" className={+user.age > 18 ? "green" : "red"}>
+//                   <div>
+//                     <div>My name's {user.name}</div>
+//                     <div>My age's {user.age}</div>
+//                   </div>
+
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
 //                 </div>
 //               );
 //             })}
@@ -111,4 +36,62 @@ export default DisplayInfor;
 //   }
 // }
 
-// export default DisplayInfor;
+const DisplayInfor = (props) => {
+  const { listUser } = props;
+  console.log(listUser);
+
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+  //Destructuring assignment
+  // this.state = {
+  //   isShowHideListUser : true
+  // }
+  const handleShowHideListUser = () => {
+    // this.setState({
+    //   isShowHideListUser: true,
+    // });
+
+    setShowHideListUser(!isShowHideListUser);
+  };
+
+  console.log(">>>call me render");
+  //giống componentDidMount, componentDidUpdate
+  useEffect(() => {
+    if (listUser.length === 0) {
+      alert("You deleted all the users!");
+    }
+    console.log(">>> call me useEffect");
+  }, [listUser]);
+
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "Hide list users" : "Show list users"}
+        </span>
+      </div>
+      {isShowHideListUser && (
+        <div>
+          {listUser &&
+            listUser.length > 0 &&
+            listUser.map((user, index) => {
+              return (
+                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                  <div>
+                    <div>My name's {user.name}</div>
+                    <div>My age's {user.age}</div>
+                  </div>
+
+                  <div>
+                    <button onClick={() => props.handleDeleteUser(user.id)}>
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
+export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,50 +1,111 @@
 //class component
 //function component
 
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
-class MyComponent extends React.Component {
-  state = {
-    listUser: [
-      { id: 1, name: "Chi", age: "16" },
-      { id: 2, name: "PC", age: "18" },
-      { id: 3, name: "Phuong Chi", age: "21" },
-    ],
+// class MyComponent extends React.Component {
+//   state = {
+//     listUser: [
+//       { id: 1, name: "Chi", age: "16" },
+//       { id: 2, name: "PC", age: "18" },
+//       { id: 3, name: "Phuong Chi", age: "21" },
+//     ],
+//   };
+
+//   handleAddNewUser = (userObj) => {
+//     // let listUserNew = this.state.listUser;
+//     // listUserNew.unshift(userObj);
+//     // this.setState({
+//     //   listUser: listUserNew,
+//     // });
+//     this.setState({
+//       listUser: [userObj, ...this.state.listUser],
+//     });
+//   };
+//   handleDeleteUser = (userId) => {
+//     let listUserClone = this.state.listUser;
+//     listUserClone = listUserClone.filter((item) => item.id !== userId);
+//     this.setState({
+//       listUser: listUserClone,
+//     });
+//   };
+//   //JSX
+//   render() {
+//     //DRY : don't repeat youseft
+//     return (
+//       <div>
+//         <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+//         <br />
+//         <br />
+//         <DisplayInfor
+//           listUser={this.state.listUser}
+//           handleDeleteUser={this.handleDeleteUser}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// const MyComponent = (props) => {
+//   // const defaultData = [
+//   //   { id: 1, name: "Chi", age: "16" },
+//   //   { id: 2, name: "PC", age: "18" },
+//   //   { id: 3, name: "Phuong Chi", age: "21" },
+//   // ];
+//   const [listUser, setListUser] = useState([
+//     { id: 1, name: "Chi", age: "16" },
+//     { id: 2, name: "PC", age: "18" },
+//     { id: 3, name: "Phuong Chi", age: "21" },
+//   ]);
+
+//   const handleAddNewUser = (userObj) => {
+//     setListUser((prevList) => [...prevList, userObj]);
+//   };
+//   const handleDeleteUser = (userId) => {
+//     let listUserClone = [...listUser]; // Tạo bản sao của listUser
+//     listUserClone = listUserClone.filter((item) => item.id !== userId);
+//     setListUser(listUserClone); // Cập nhật lại state với mảng mới
+//   };
+
+//   return (
+//     <div>
+//       <AddUserInfor handleAddNewUser={handleAddNewUser} />
+//       <br />
+//       <br />
+//       <DisplayInfor listUser={listUser} handleDeleteUser={handleDeleteUser} />
+//     </div>
+//   );
+// };
+const MyComponent = (props) => {
+  // const defaultData = [
+  //   { id: 1, name: "Chi", age: "16" },
+  //   { id: 2, name: "PC", age: "18" },
+  //   { id: 3, name: "Phuong Chi", age: "21" },
+  // ];
+  const [listUser, setListUser] = useState([
+    { id: 1, name: "Chi", age: "16" },
+    { id: 2, name: "PC", age: "18" },
+    { id: 3, name: "Phuong Chi", age: "21" },
+  ]);
+
+  const handleAddNewUser = (userObj) => {
+    setListUser([...listUser, userObj]);
+  };
+  const handleDeleteUser = (userId) => {
+    let listUserClone = listUser; // Tạo bản sao của listUser
+    listUserClone = listUserClone.filter((item) => item.id !== userId);
+    setListUser(listUserClone); // Cập nhật lại state với mảng mới
   };
 
-  handleAddNewUser = (userObj) => {
-    // let listUserNew = this.state.listUser;
-    // listUserNew.unshift(userObj);
-    // this.setState({
-    //   listUser: listUserNew,
-    // });
-    this.setState({
-      listUser: [userObj, ...this.state.listUser],
-    });
-  };
-  handleDeleteUser = (userId) => {
-    let listUserClone = this.state.listUser;
-    listUserClone = listUserClone.filter((item) => item.id !== userId);
-    this.setState({
-      listUser: listUserClone,
-    });
-  };
-  //JSX
-  render() {
-    //DRY : don't repeat youseft
-    return (
-      <div>
-        <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
-        <br />
-        <br />
-        <DisplayInfor
-          listUser={this.state.listUser}
-          handleDeleteUser={this.handleDeleteUser}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <AddUserInfor handleAddNewUser={handleAddNewUser} />
+      <br />
+      <br />
+      <DisplayInfor listUser={listUser} handleDeleteUser={handleDeleteUser} />
+    </div>
+  );
+};
 export default MyComponent;
